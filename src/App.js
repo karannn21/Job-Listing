ae61c024159558411d8329841032f9cd180005b4
karannn21 <karansharma9038@gmail.com>
2025-07-03 01:37:00 +0530

    Fixed sticky logo issue for mobile devices

--- src/App.js
+++ src/App.js
@@ -258,7 +258,7 @@ function App() {
   };
   const overlayStyle = {
     backgroundColor: "rgba(255, 255, 255, 0.7)",
-    position: "sticky",
+    position: "relative",
     top: 0,
     left: 0,
     right: 0,
